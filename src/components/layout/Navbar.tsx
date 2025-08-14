9e86fb93ca027c51c213d3011b88fca7aa111581
Shakil Ahmed <shakilahmed5161@gmail.com>
2025-08-14 20:36:24 +0600

    1 Implementing Dark Theme Toggle
    2 Refactoring Layout
    
    3 Integrating React Hook Form with ShadCN Components
    
    4 Form Validation Using Zod
    
    5 Building the User Registration Form
    
    6 Enhancing Password and Confirm Password Components
    
    7 Setting Up Redux for State Management
    
    8 Configuring Axios and Creating axiosBaseQuery
    
    9 Handling User Account Creation
    
    10 Understanding the Registration Flow with Verification
    
    11 Visualizing axiosBaseQuery Payload and Router State

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,20 +2,20 @@
 import Logo from "@/assets/icons/Logo";
 import { Button } from "@/components/ui/button";
 import {
-    NavigationMenu,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
 } from "@/components/ui/navigation-menu";
+import { Link } from "react-router";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { ModeToggle } from "./ModeToggler";
 
 
 // Navigation links array to be used in both desktop and mobile menus
 const navigationLinks = [
-  { href: "#", label: "Home", active: true },
-  { href: "#", label: "Features" },
-  { href: "#", label: "Pricing" },
-  { href: "#", label: "About" },
+  { href: "/", label: "Home" },
+  { href: "/about", label: "About" },
 ];
 
 export default function Navbar() {
@@ -65,11 +65,11 @@ export default function Navbar() {
                   {navigationLinks.map((link, index) => (
                     <NavigationMenuItem key={index} className="w-full">
                       <NavigationMenuLink
-                        href={link.href}
                         className="py-1.5"
-                        active={link.active}
+                        asChild
                       >
-                        {link.label}
+                       <Link to={link.href}>{link.label}</Link> 
+                       
                       </NavigationMenuLink>
                     </NavigationMenuItem>
                   ))}
@@ -88,11 +88,10 @@ export default function Navbar() {
                 {navigationLinks.map((link, index) => (
                   <NavigationMenuItem key={index}>
                     <NavigationMenuLink
-                      active={link.active}
-                      href={link.href}
+                      asChild
                       className="text-muted-foreground hover:text-primary py-1.5 font-medium"
                     >
-                      {link.label}
+                      <Link to={link.href}>{link.label}</Link>
                     </NavigationMenuLink>
                   </NavigationMenuItem>
                 ))}
@@ -102,11 +101,9 @@ export default function Navbar() {
         </div>
         {/* Right side */}
         <div className="flex items-center gap-2">
-          <Button asChild variant="ghost" size="sm" className="text-sm">
-            <a href="#">Sign In</a>
-          </Button>
-          <Button asChild size="sm" className="text-sm">
-            <a href="#">Get Started</a>
+          <ModeToggle />
+          <Button asChild  className="text-sm">
+            <Link to='/login' >Login</Link>
           </Button>
         </div>
       </div>
---
+++ src/pages/Login.tsx
@@ -0,0 +1,29 @@
+import Logo from "@/assets/icons/Logo";
+import { LoginForm } from "@/components/modules/Authentication/LoginForm";
+import { Link } from "react-router";
+
+export default function Login() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <Link to="/" className="flex items-center gap-2 font-medium">
+            <Logo />
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <img
+          src='https://cdn.pixabay.com/photo/2024/06/18/04/33/travel-8836969_1280.jpg'
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.8]"
+        />
+      </div>
+    </div>
+  );
+}
